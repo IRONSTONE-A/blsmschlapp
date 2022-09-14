865d5366282f54ac1ece859c86d1bd6bf10292b4
6019thinkingman <99737495+6019thinkingman@users.noreply.github.com>
2022-09-14 11:51:49 +0300

    first commit

---
+++ src/routers/AppRouter.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes, Outlet, Navigate } from "react-router-dom"
+import Navbar from "../components/Navbar"
+import Home from "../pages/Home"
+import Login from "../pages/Login"
+import Register from "../pages/Register"
+
+
+
+const AppRouter = () => {
+   
+  return (
+    <BrowserRouter>
+        <Navbar/>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="register/" element={<Register/>}/>
+            <Route path="login/" element={<Login/>}/>
+            
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AppRouter
